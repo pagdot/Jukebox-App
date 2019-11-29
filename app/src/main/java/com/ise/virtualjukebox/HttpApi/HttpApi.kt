34e1bf7b8622296479c5f6e0576fccb5f65fb34d
Mathias Dittrich <MathiasDittrich@gmx.at>
2019-11-29 17:14:06 +0100

    2019-11-29 +Update: Test non-blocking getGithub request
    +Update: Test non-blocking getGithub request: getGithub gets called and ques the get request but returns immediately to the main. It will get back to the handler after finishing communication.

--- app/src/main/java/com/ise/virtualjukebox/HttpApi/HttpApi.kt
+++ app/src/main/java/com/ise/virtualjukebox/HttpApi/HttpApi.kt
@@ -1,4 +1,30 @@
 package com.ise.virtualjukebox.HttpApi
 
+import android.util.Log
+import okhttp3.Response
+import org.json.JSONArray
+import java.io.IOException
+
 class HttpApi {
+    private var baseUrl : String = "/api/v1/"
+    private var sessionID = null
+    private var restClient = RestClient()
+
+    private fun getSessionID() {
+        //restClient.queueGetCall()
+    }
+
+    fun getGithub() {
+        restClient.queueGetCall("https://api.github.com/users/MADITT/repos", object : RestClient.HttpCallback {
+            override fun onSuccess(response: Response) {
+                Log.e("Volley", "Invalid JSON Object.")
+                val jsonDataString = response.body?.string()
+                val jsonDataArray = JSONArray(jsonDataString)
+            }
+
+            override fun onFailure(response: Response?, exception: IOException?) {
+                Log.e("Volley", "Invalid JSON Object.")
+            }
+        })
+    }
 }
--- app/src/main/java/com/ise/virtualjukebox/MainActivity.kt
+++ app/src/main/java/com/ise/virtualjukebox/MainActivity.kt
@@ -2,11 +2,18 @@ package com.ise.virtualjukebox
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ise.virtualjukebox.HttpApi.HttpApi
 
 class MainActivity : AppCompatActivity() {
+
+    var api = HttpApi()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        api.getGithub()
+
+        var test = 1
     }
 }
