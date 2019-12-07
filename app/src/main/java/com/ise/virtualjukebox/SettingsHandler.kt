66367717ab469e8972329be42911d4ecd326dd00
Tobi <egtobi@gmail.com>
2019-12-07 18:47:48 +0100

    Added untested SettingsHandler

---
+++ app/src/main/java/com/ise/virtualjukebox/SettingsHandler.kt
@@ -0,0 +1,40 @@
+package com.ise.virtualjukebox
+
+import java.io.IOException
+
+class SettingsHandler{
+    private var _MainHandler: MainHandler;
+
+    constructor(MainInstance : MainHandler){
+        _MainHandler = MainInstance;
+    }
+
+
+    interface SettingsHandlerCallback {
+        /**
+         *
+         * @param statusCode - contains the rest status code if the response was not OK (200). exception is then null
+         * @param exception - contains the exception. statusCode is then null
+         */
+        fun onFailure(status : String?, exception : IOException?)
+
+        fun onSuccess()
+    }
+
+    fun Connect(ServerIP: String) : Boolean{
+        _MainHandler.DisconnectAllServer();
+        return _MainHandler.ConnectToExistingServer(ServerIP);
+    }
+    fun FetchServerList() : MutableList<MainHandler.ServerPair>?{
+        return _MainHandler.ServList;
+    }
+    fun AddServer(Name: String, ServerIP: String, CB:SettingsHandlerCallback) : MutableList<MainHandler.ServerPair>?{
+        _MainHandler.DisconnectAllServer();
+        if(_MainHandler.CreateNewServer(Name, ServerIP)) {
+            CB.onSuccess();
+        }else{
+            CB.onFailure("Failed to Add Server", null);
+        }
+        return FetchServerList();
+    }
+}
