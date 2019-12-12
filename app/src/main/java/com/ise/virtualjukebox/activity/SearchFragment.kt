df25940f4950869032c5a8b00bd9407f5fcbcf94
David <david.bv@aon.at>
2019-12-12 20:52:16 +0100

    fixed broken search result item layout

--- app/src/main/java/com/ise/virtualjukebox/activity/SearchFragment.kt
+++ app/src/main/java/com/ise/virtualjukebox/activity/SearchFragment.kt
@@ -25,7 +25,7 @@ class SearchFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search_linear, container, false)
+        return inflater.inflate(R.layout.fragment_search, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
